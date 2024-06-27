371f86c68f6f4cb56b45205b5e19ee7946497f83
24LP0056 24LP0056 <24LP0056@G301-CC0198.local>
2024-06-27 17:52:50 +0800

    text typed

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,8 +15,9 @@ export const Header = () => {
         <div className="">
           <p className="text-4xl text-white">Hi, Im' Toji</p>
           <p className="text-base text-white">
-            I'm a full stack developer (React.js & Node.js) with a focus on
-            creating (and occasionally designing) exceptional digital
+            Oreno namewa Toji desu. Kore sitewa Degisanga oreni tsukattandesu.
+            Mite kureta arigatogozaimashita. (React.js & Node.js) with a focus
+            on creating (and occasionally designing) exceptional digital
             experiences that are fast, accessible, visually appealing, and
             responsive. Even though I have been creating web applications for
             over 7 years, I still love it as if it was something new.
